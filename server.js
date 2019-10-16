01052450a2d58518d4b887d570de9c60d0a3fafc
chenfuye <chenfuye@vanvy.cn>
2019-10-16 21:06:41 +0800

    first

---
+++ server.js
@@ -0,0 +1,14 @@
+let http = require('http')
+let users = [{ id: 1, name: 'fuck' }, { id: 2, name: 'kimi' }]
+let server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if (req.url === '/api/users') {
+    res.end(JSON.stringify(users))
+  } else {
+    res.end('not found')
+  }
+})
+
+server.listen(3000, () => {
+  console.log('服务启动成功')
+})
